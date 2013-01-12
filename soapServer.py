5740cbfa72fa5e6ab4df8cc381216030b1c2270e
unknown <SLachterman@msn-slachterman.edge.local>
2013-01-12 17:26:26 -0600

    new tns

--- soapServer.py
+++ soapServer.py
@@ -7,6 +7,8 @@ app = Flask(__name__)
 enterprise = Enterprise(app)
 
 class DemoService(enterprise.SOAPService):
+    __soap_target_namespace__ = 'http://soap.sforce.com/2005/09/outbound'
+    
     @enterprise.soap(_returns=enterprise._sp.String)
     def get_time(self):
         return ctime()
@@ -18,7 +20,7 @@ class DemoService(enterprise.SOAPService):
             results.append('Hello, %s' % name)
         return results
     
-    @enterprise.soap(_returns=enterprise._sp.String, __soap_target_namespace__ = "http://soap.sforce.com/2005/09/outbound")
+    @enterprise.soap(_returns=enterprise._sp.String)
     def notifications(self):
         return 'Yep.'
 
