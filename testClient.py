dd6c9380b424cffe61d21ab7c21c04f67353da89
Sam Lachterman <Slachterman@gmail.com>
2013-03-10 09:34:59 -0500

    Update testClient.py

--- testClient.py
+++ testClient.py
@@ -3,7 +3,4 @@ import suds
 url = 'http://thawing-forest-6142.herokuapp.com/_enterprise/soap?wsdl'
 client = suds.client.Client(url)
 
-sTime = client.service.get_time()
-print sTime
-
 print client.service.notifications()
